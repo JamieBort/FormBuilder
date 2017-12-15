7616d78be39b7368c54e74f4a824b77b60f7f3e5
Jamie Bort <jamiebort@gmail.com>
2017-12-15 11:01:45 -0500

    lost track of what changed

--- js/main.js
+++ js/main.js
@@ -111,7 +111,9 @@ console.log(formData[ 4 ]);
 console.log(formData[ 4 ].label);
 console.log(formData[ 4 ].id);
 console.log(formData[ 4 ].options);
+console.log("what is this");
 console.log(formData[ 4 ].options[ 0 ]);
+console.log("what is this?");
 console.log(formData[ 4 ].options[ 0 ].label);
 console.log(formData[ 4 ].options[ 1 ].label);
 console.log(formData[ 4 ].options[ 4 ].label);
@@ -134,26 +136,40 @@ let header = document.createElement( "div" );
 let title = document.createElement( "span" );
 let fields = document.createElement( "div" );
 
+
+
+for (var i = 0; i < formData.length; i++) {
+//   let formData[i].id+Div = document.createElement( "div" );
+//   let formData[i].id+_i = document.createElement( "i" );
+  // let formData[i].id = document.createElement( "input" );
+}
+
+// formData[ 0 ].id = user-first-name
 let firstNameDiv = document.createElement( "div" );
 let firstName_i = document.createElement( "i" );
 let firstName = document.createElement( "input" );
 
+// formData[ 1 ].id = user-last-name
 let lastNameDiv = document.createElement( "div" );
 let lastName_i = document.createElement( "i" );
 let lastName = document.createElement( "input" );
 
+// formData[ 2 ].id = email
 let emailDiv = document.createElement( "div" );
 let email_i = document.createElement( "i" );
 let email = document.createElement( "input" );
 
+// formData[ 3 ].id = user-website
 let urlDiv = document.createElement( "div" );
 let url_i = document.createElement( "i" );
 let url = document.createElement( "input" );
 
+// formData[ 4 ].id = user-language
 let selectDiv = document.createElement( "div" );
 let select_i = document.createElement( "i" );
 let select = document.createElement( "select" );
 
+// formData[ 4 ].options[ i ].label
 let firstOption = document.createElement( "Option" );
 let secondOption = document.createElement( "Option" );
 let thirdOption = document.createElement( "Option" );
@@ -161,20 +177,23 @@ let forthOption = document.createElement( "Option" );
 let fifthOption = document.createElement( "Option" );
 let sixthOption = document.createElement( "Option" );
 
+// formData[ 5 ].id = user-comment
 let textAreaDiv = document.createElement( "div" );
 let textArea_i = document.createElement( "i" );
 let textArea = document.createElement( "textarea" );
 
+// formData[ 6 ].id = mobile
 let mobileDiv = document.createElement( "div" );
 let mobile_i = document.createElement( "i" );
 let mobile = document.createElement( "input" );
 
+// formData[ 7 ].id = home
 let homeDiv = document.createElement( "div" );
 let home_i = document.createElement( "i" );
 let home = document.createElement( "input" );
 
 let footer = document.createElement( "div" ); // Nothing to iterate over.
-let button = document.createElement( "button" );
+let button = document.createElement( "button" ); // Nothing to iterate over.
 
 // Create all of the text nodes
 let spanText = document.createTextNode( "Sign Up For My Web App" ); // Nothing to iterate over.
@@ -187,17 +206,18 @@ let fifthOptionText = document.createTextNode( "Chinese" ); // formData[ 4 ].opt
 let sixthOptionText = document.createTextNode( "Japanese" ); // formData[ 4 ].options[ 4 ].label
 
 // Set any needed attribute values for the HTML nodes
-wrapper.setAttribute('class', 'wrapper');
-header.setAttribute('class', 'header');
-title.setAttribute('class', 'title');
-fields.setAttribute('class', 'fields');
-fields.setAttribute('id', 'fields');
+
+// wrapper.setAttribute('class', 'wrapper');
+// header.setAttribute('class', 'header');
+// title.setAttribute('class', 'title');
+// fields.setAttribute('class', 'fields');
+// fields.setAttribute('id', 'fields');
 
 firstNameDiv.setAttribute('class', 'div_class'); // Nothing to iterate over.
 firstName.setAttribute( 'type', 'text' ); // formData[ 0 ].type
 firstName.setAttribute( 'id', 'user-first-name' ); // formData[ 0 ].id
 firstName.setAttribute('class', 'input_class'); // Nothing to iterate over.
-firstName.setAttribute( 'placeholder', 'First Name' );
+firstName.setAttribute( 'placeholder', 'First Name' ); // Nothing to iterate over.
 firstName_i.setAttribute('class', 'fa fa-user'); // formData[ 0 ].icon
 firstName_i.setAttribute('aria-hidden', 'true'); // Nothing to iterate over.
 
@@ -205,15 +225,15 @@ lastNameDiv.setAttribute('class', 'div_class'); // Nothing to iterate over.
 lastName.setAttribute( 'type', 'text' ); // formData[ 1 ].type
 lastName.setAttribute( 'id', 'user-last-name' ); // formData[ 1 ].id
 lastName.setAttribute('class', 'input_class'); // Nothing to iterate over.
-lastName.setAttribute( 'placeholder', 'Last Name' );
-lastName_i.setAttribute('class', 'fa fa-user-o'); // formData[ 1 ].icon
+lastName.setAttribute( 'placeholder', 'Last Name' ); // Nothing to iterate over.
+lastName_i.setAttribute('class', 'fa fa-user'); // formData[ 1 ].icon
 lastName_i.setAttribute('aria-hidden', 'true'); // Nothing to iterate over.
 
 emailDiv.setAttribute('class', 'div_class'); // Nothing to iterate over.
 email.setAttribute( 'type', 'email' ); // formData[ 2 ].type
 email.setAttribute( 'id', 'user-email' ); // formData[ 2 ].id
 email.setAttribute('class', 'input_class'); // Nothing to iterate over.
-email.setAttribute( 'placeholder', 'Email Address' );
+email.setAttribute( 'placeholder', 'Email Address' ); // Nothing to iterate over.
 email_i.setAttribute('class', 'fa fa-envelope'); // formData[ 2 ].icon
 email_i.setAttribute('aria-hidden', 'true'); // Nothing to iterate over.
 
@@ -221,7 +241,7 @@ urlDiv.setAttribute('class', 'div_class'); // Nothing to iterate over.
 url.setAttribute( 'type', 'text' ); // formData[ 3 ].type
 url.setAttribute( 'id', 'user-website' ); // formData[ 3 ].id
 url.setAttribute('class', 'input_class'); // Nothing to iterate over.
-url.setAttribute( 'placeholder', 'Current Website URL' );
+url.setAttribute( 'placeholder', 'Current Website URL' ); // Nothing to iterate over.
 url_i.setAttribute('class', 'fa fa-link'); // formData[ 3 ].icon
 url_i.setAttribute('aria-hidden', 'true'); // Nothing to iterate over.
 
@@ -250,7 +270,7 @@ textArea.setAttribute( 'rows', '8' ); // Nothing to iterate over.
 textArea.setAttribute( 'cols', '80' ); // Nothing to iterate over.
 textArea.setAttribute( 'id', 'user-comment' ); // formData[ 5 ].id
 textArea.setAttribute('class', 'input_class'); // Nothing to iterate over.
-textArea.setAttribute( 'placeholder', 'Your Comment' );
+textArea.setAttribute( 'placeholder', 'Your Comment' ); // Nothing to iterate over.
 textArea_i.setAttribute('class', 'fa fa-pencil'); // formData[ 5 ].icon
 textArea_i.setAttribute('aria-hidden', 'true'); // Nothing to iterate over.
 
@@ -258,7 +278,7 @@ mobileDiv.setAttribute('class', 'div_class'); // Nothing to iterate over.
 mobile.setAttribute( 'type', 'tel' ); // formData[ 6 ].type
 mobile.setAttribute( 'id', 'user-mobile' ); // formData[ 6 ].id
 mobile.setAttribute('class', 'input_class'); // Nothing to iterate over.
-mobile.setAttribute( 'placeholder', 'Mobile Number' );
+mobile.setAttribute( 'placeholder', 'Mobile Number' ); // Nothing to iterate over.
 mobile_i.setAttribute('class', 'fa fa-mobile-phone'); // formData[ 6 ].icon
 mobile_i.setAttribute('aria-hidden', 'true'); // Nothing to iterate over.
 
@@ -266,7 +286,7 @@ homeDiv.setAttribute('class', 'div_class'); // Nothing to iterate over.
 home.setAttribute( 'type', 'tel' ); // formData[ 7 ].type
 home.setAttribute( 'id', 'user-phone' ); // formData[ 7 ].id
 home.setAttribute('class', 'input_class'); // Nothing to iterate over.
-home.setAttribute( 'placeholder', 'Home Number' );
+home.setAttribute( 'placeholder', 'Home Number' ); // Nothing to iterate over.
 home_i.setAttribute('class', 'fa fa-phone'); // formData[ 7 ].icon
 home_i.setAttribute('aria-hidden', 'true'); // Nothing to iterate over.
 
